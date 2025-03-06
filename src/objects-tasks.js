fe2bbf5120b920706b31626dbefb4fe9ffbdac5e
Vladislav Naftolin <vnaftolin@mail.ru>
2025-03-06 21:06:28 +0300

    feat: implement group function

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -137,38 +137,17 @@ function sortCitiesArray(arr) {
   });
 }
 
-/**
- * Groups elements of the specified array by key.
- * Returns multimap of keys extracted from array elements via keySelector callback
- * and values extracted via valueSelector callback.
- * See: https://en.wikipedia.org/wiki/Multimap
- *
- * @param {array} array
- * @param {Function} keySelector
- * @param {Function} valueSelector
- * @return {Map}
- *
- * @example
- *   group([
- *      { country: 'Belarus', city: 'Brest' },
- *      { country: 'Russia', city: 'Omsk' },
- *      { country: 'Russia', city: 'Samara' },
- *      { country: 'Belarus', city: 'Grodno' },
- *      { country: 'Belarus', city: 'Minsk' },
- *      { country: 'Poland', city: 'Lodz' }
- *     ],
- *     item => item.country,
- *     item => item.city
- *   )
- *            =>
- *   Map {
- *    "Belarus" => ["Brest", "Grodno", "Minsk"],
- *    "Russia" => ["Omsk", "Samara"],
- *    "Poland" => ["Lodz"]
- *   }
- */
-function group(/* array, keySelector, valueSelector */) {
-  throw new Error('Not implemented');
+function group(array, keySelector, valueSelector) {
+  const resultMap = new Map();
+  array.forEach((item) => {
+    const country = keySelector(item);
+    const city = valueSelector(item);
+    if (!resultMap.has(country)) {
+      resultMap.set(country, []);
+    }
+    resultMap.get(country).push(city);
+  });
+  return resultMap;
 }
 
 /**
